6ef95aefb3a574e8403e99d450f597f118b31eec
Uday singh 1 <91188742+Uday-Singh1@users.noreply.github.com>
2022-04-08 18:16:44 +0200

    first commit

---
+++ duurzaamhuis/code faissal/data.js
@@ -0,0 +1,145 @@
+/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
+const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+
+"use strict"
+const timeDelay = 250; // time delay refresh data
+let refreshTimer = window.setInterval(renderData, timeDelay); // timer data opvragen van server
+
+// jouw persoonlijke URL
+const mijnDataURL = "https://data.softwaredeveloper.amsterdam/api/device/98b08e4d/latest"; 
+
+const dataDiv = document.getElementById("dataDiv"); // hier komt de data
+
+async function getSensorData() {
+    let url = mijnDataURL;
+    try {
+        let response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderData() {
+        let measurementRaw = await getSensorData();
+        let measurement = measurementRaw.data[0];
+        console.log(measurement);
+
+         // Nieuwe Javascript date maken met de de datum en tijd van de meting
+         let datum =  new Date(measurement.datum);
+
+         // De meting gegevens wegschrijven naar de div
+       //  dataDiv.innerHTML = "<h3>Datum: ${datum.toLocaleString()}</h3><p>Sensor: ${measurement.sensor}   dataDiv.innerHTML = "<h3>Datum: ${datum.toLocaleString()}</h3><p>Sensor: ${measurement.sensor}</p>
+  
+}
+    /*
+
+renderData(); // start immediately
+
+
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      //athens,gr
+      if (inputVal.includes(",")) {
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        //athens
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well 😉`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
+*/
+
+const data = {
+    labels: ['Gas','Elektra','Water'],
+    datasets:[
+        {
+            label: "Energie Verbruik",
+            data: [4000, 5000 , 3000,],
+            backgroundColor: ['#29EE99','#1AAE6E','#45b3e7',],
+        }
+    ]
+}
+const config = {
+  type: 'bar',
+  data: data,
+  maintainAspectRatio: true,
+}
+
+
+new Chart(document.getElementById("js--chart--1"), config);
+
+
+
